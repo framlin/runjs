d97a4e288fedaf4ae76f585cab178a2c14a18fa9
framlin <w.egger@framlin.com>
2015-01-30 18:01:18 +0100

    introduce Sections, to encapsulate section-stuff

--- site/behave/lense.js
+++ site/behave/lense.js
@@ -1,67 +1,22 @@
-function Section() {
-
-};
-
-$(function(){
-    var checkScroll = true;
-    var $secNavigation = $('#sec-navigation');
-    var $sectionNavigation = $('<nav id="section-navigation"><ul></ul></nav>');
-    var $navigationList = $('ul', $sectionNavigation);
-    var $lastMasterSection;
-
-    function getSectionId($section) {
+function Sections($, $sections) {
+    var me = this;
+    this.getSectionId = function getSectionId($section) {
         return $('> header > h1', $section).attr('id');
-    }
+    };
 
-    function getSectionNavEntry($section){
-        return $('#li-'+ getSectionId($section))
-    }
+    this.getSectionNavEntry = function getSectionNavEntry($section){
+        return $('#li-'+ this.getSectionId($section))
+    };
 
-
-    function appendSectionNavigationEntry($navList) {
-        return function addEntry() {
-            var sectTitle = $('> header > h1', this).text();
-            var secID = getSectionId(this);
-            var $li = $('<li>' + sectTitle + '</li>');
-            $li.attr('id', 'li-'+ secID);
-            $navList.append($li);
-        }
-    }
-
-    function fillSectionNavigation($navList) {
-        $('section').each(appendSectionNavigationEntry($navList));
-    }
-
-
-    function scrollWatcher(event) {
-        if (checkScroll){
-            setTimeout(augmentVisibleSections(), 100);
-        }
-    }
-
-
-    function isVisible(viewportHeight, top, height) {
+    this.isVisible = function isVisible(viewportHeight, top, height) {
         var viewportTop = 0,
             topIntersects = (top > viewportTop) && (top < (viewportTop + viewportHeight)),
             bottomIntersects = ((top +height) > viewportTop) && ((top +height)  < (viewportTop + viewportHeight));
 
         return topIntersects || bottomIntersects;
-    }
+    };
 
-    function augmentSectionNavigation() {
-        var offset = $(this).offset(),
-            posY = offset.top - $(window).scrollTop(),
-            height = $(this).height();
-
-
-        if (isVisible($(window).height(), posY, height)) {
-            $(this).trigger('startVisibleSection');
-        } else {
-            $(this).trigger('finishVisibleSection');
-        }
-    }
-
-    function getVisibleHeight($section) {
+    this.getVisibleHeight = function getVisibleHeight($section) {
         var offset = $section.offset();
         var posY = offset.top - $(window).scrollTop();
         var height = $section.height();
@@ -85,18 +40,128 @@ $(function(){
 
         return result;
 
+    };
+
+    this.onStartMasterSection = function onStartMasterSection(event) {
+        var $sectionNavigationEntry = me.getSectionNavEntry($(this)),
+            $sectionHeading = $('> header', $(this)).clone(),
+            $sectionContext = $('#section-context');
+
+        $sectionNavigationEntry.addClass('master-section');
+
+
+        $sectionHeading.appendTo($sectionContext);
+        $('.section-context', $(this)).clone().appendTo($sectionContext);
+        $(this).addClass('master-section');
+    };
+
+    this.onFinishMasterSection = function onFinishMasterSection(event) {
+        var $sectionNavigationEntry = me.getSectionNavEntry($(this)),
+            $sectionContext = $('#section-context'),
+            $sectionContextHeading = $('> header', $sectionContext);
+
+        if ($sectionNavigationEntry.hasClass('master-section')) {
+            $sectionNavigationEntry.removeClass('master-section');
+        }
+        $sectionNavigationEntry.addClass('visible-section');
+        $sectionContextHeading.remove();
+
+
+        $('.section-context', $sectionContext).remove();
+        if ($(this).hasClass('master-section')) {
+            $(this).removeClass('master-section');
+        }
+    };
+
+    this.onStartVisibleSection = function onStartVisibleSection(event) {
+        var $sectionNavigationEntry;
+
+        if (!$(this).hasClass('master-section')) {
+            $sectionNavigationEntry = me.getSectionNavEntry($(this));
+            $sectionNavigationEntry.addClass('visible-section');
+        }
+    };
+
+    this.onFinishVisibleSection = function onFinishVisibleSection(event) {
+        var $sectionNavigationEntry = me.getSectionNavEntry($(this));
+
+        if ($sectionNavigationEntry.hasClass('visible-section')) {
+            $sectionNavigationEntry.removeClass('visible-section');
+        }
+
+    };
+
+    this.initSections = function initSections($sections) {
+        $sections.each(function () {
+            $(this).on('startVisibleSection', me.onStartVisibleSection);
+            $(this).on('finishVisibleSection', me.onFinishVisibleSection);
+            $(this).on('startMasterSection', me.onStartMasterSection);
+            $(this).on('finishMasterSection', me.onFinishMasterSection);
+        });
+    };
+
+    this.appendSectionNavigationEntry = function appendSectionNavigationEntry($navList) {
+        return function addEntry() {
+            var sectTitle = $('> header > h1', this).text();
+            var secID = me.getSectionId(this);
+            var $li = $('<li>' + sectTitle + '</li>');
+            $li.attr('id', 'li-'+ secID);
+            $navList.append($li);
+        }
+    };
+
+    this.fillSectionNavigation = function fillSectionNavigation($navList) {
+        $('section').each(me.appendSectionNavigationEntry($navList));
+    };
+
+
+
+    this.initSections($sections);
+
+}
+
+$(function(){
+    var checkScroll = true,
+        $secNavigation = $('#sec-navigation'),
+        $sectionNavigation = $('<nav id="section-navigation"><ul></ul></nav>'),
+        $navigationList = $('ul', $sectionNavigation),
+        $lastMasterSection,
+        sections = new Sections($, $('section'));
+
+
+
+    function scrollWatcher(event) {
+        if (checkScroll){
+            setTimeout(augmentVisibleSections(), 100);
+        }
     }
+
+
+
+    function augmentSectionNavigation() {
+        var offset = $(this).offset(),
+            posY = offset.top - $(window).scrollTop(),
+            height = $(this).height();
+
+
+        if (sections.isVisible($(window).height(), posY, height)) {
+            $(this).trigger('startVisibleSection');
+        } else {
+            $(this).trigger('finishVisibleSection');
+        }
+    }
+
 
     function augmentMasterSection($sections) {
         var mostVisible = {area: 0, section: null, top:$(window).height() +100},
             masterSecID = null,
-            lastMasterSecID = getSectionId($lastMasterSection);
+            lastMasterSecID = sections.getSectionId($lastMasterSection);
 
         function computeMostVisible() {
             var offset = $(this).offset(),
                 posY = offset.top - $(window).scrollTop(),
                 height = $(this).height(),
-                visibleHeight = getVisibleHeight($(this)),
+                visibleHeight = sections.getVisibleHeight($(this)),
                 area = (visibleHeight/height)*100;
 
 
@@ -115,7 +180,7 @@ $(function(){
 
         $sections.each(computeMostVisible);
 
-        masterSecID = getSectionId(mostVisible.section);
+        masterSecID = sections.getSectionId(mostVisible.section);
         if (lastMasterSecID != masterSecID) {
             if ($lastMasterSection) {
                 $lastMasterSection.trigger('finishMasterSection');
@@ -137,76 +202,21 @@ $(function(){
         }
     }
 
-    function onStartMasterSection(event) {
-        var $sectionNavigationEntry = getSectionNavEntry($(this)),
-            $sectionHeading = $('> header', $(this)).clone(),
-            $sectionContext = $('#section-context');
-
-        $sectionNavigationEntry.addClass('master-section');
 
 
-        $sectionHeading.appendTo($sectionContext);
-        $('.section-context', $(this)).clone().appendTo($sectionContext);
-        $(this).addClass('master-section');
-    }
 
-    function onFinishMasterSection(event) {
-        var $sectionNavigationEntry = getSectionNavEntry($(this)),
-            $sectionContext = $('#section-context'),
-            $sectionContextHeading = $('> header', $sectionContext);
-
-        if ($sectionNavigationEntry.hasClass('master-section')) {
-            $sectionNavigationEntry.removeClass('master-section');
-        }
-        $sectionNavigationEntry.addClass('visible-section');
-        $sectionContextHeading.remove();
-
-
-        $('.section-context', $sectionContext).remove();
-        if ($(this).hasClass('master-section')) {
-            $(this).removeClass('master-section');
-        }
-    }
-
-    function onStartVisibleSection(event) {
-        var $sectionNavigationEntry;
-
-        if (!$(this).hasClass('master-section')) {
-            $sectionNavigationEntry = getSectionNavEntry($(this));
-            $sectionNavigationEntry.addClass('visible-section');
-        }
-    }
-
-    function onFinishVisibleSection(event) {
-        var $sectionNavigationEntry = getSectionNavEntry($(this));
-
-        if ($sectionNavigationEntry.hasClass('visible-section')) {
-            $sectionNavigationEntry.removeClass('visible-section');
-        }
-
-    }
-
-    function initSections($sections) {
-        $sections.each(function () {
-            $(this).on('startVisibleSection', onStartVisibleSection);
-            $(this).on('finishVisibleSection', onFinishVisibleSection);
-            $(this).on('startMasterSection', onStartMasterSection);
-            $(this).on('finishMasterSection', onFinishMasterSection);
-        });
-    }
 
 
     $secNavigation.width(200);
     $secNavigation.append($sectionNavigation);
+    $($secNavigation).width($(window).width() - 20);
 
-    initSections($('section'));
-    fillSectionNavigation($navigationList);
+    sections.fillSectionNavigation($navigationList);
 
     $(window).scroll(scrollWatcher);
 
     augmentVisibleSections()();
 
-    $($secNavigation).width($(window).width() - 20);
 
     $('#section-context').top(+($($secNavigation).top())+20);
 });
